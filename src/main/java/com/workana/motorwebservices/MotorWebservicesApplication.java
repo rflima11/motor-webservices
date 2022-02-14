99d9b05ea8c4b608b4af8e37c5f1b934a6487ad7
Rodolfo Ferreira de Lima <rodolfo.lima.ferreira11@gmail.com>
2022-02-14 19:06:54 -0300

    first commit

---
+++ src/main/java/com/workana/motorwebservices/MotorWebservicesApplication.java
@@ -0,0 +1,13 @@
+package com.workana.motorwebservices;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MotorWebservicesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MotorWebservicesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/workana/motorwebservices/MotorWebservicesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.workana.motorwebservices;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MotorWebservicesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
